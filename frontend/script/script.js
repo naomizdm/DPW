a8b105114f488bb504137a25b43b68815691bba2
naomizdm <naomizdm@gmail.com>
2022-03-15 03:25:18 -0700

    first commit

---
+++ frontend/script/script.js
@@ -0,0 +1,14 @@
+console.log(document);
+/*var, let, const, typeof (cari jenis) . array, looping*/
+let a = 6/2;
+if(a==3)console.log("benar")
+else console.log("waduh salah")
+
+const h1=document.querySelector(".data h1"); // h1 dari kelas data
+h1.style.color="purple";
+
+const h3=document.querySelector(".data h3") // h3 dari kelas data
+h3.style.background="grey";
+h3.style.color="black";
+
+//manipulation node//
